1fa2b4ca7209697a081948ebe4a3183448e3f6e7
amiry-taha <126777345+amiry-taha@users.noreply.github.com>
2024-01-06 20:08:00 +0100

    Add files via upload

---
+++ demo-K/src/test/java/org/sid/demospringcloudstreamskafka/DemoKApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.demospringcloudstreamskafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoKApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
